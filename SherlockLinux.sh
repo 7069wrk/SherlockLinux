3c32c613b64ecf8eebb92c923d20926eeca3a105
7069wrk <102612356+7069wrk@users.noreply.github.com>
2025-04-19 06:16:04 -0500

    Update SherlockLinux.sh

--- SherlockLinux.sh
+++ SherlockLinux.sh
@@ -432,7 +432,7 @@ cd  $INSTDIR/infoga/
 sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev
 curl -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash
 ~/_installed/sherlock
-echo "export MY_Sherlock=\"$HOME/_installed/sherlock\"" >> -a .bashrc 
+echo "export MY_SHERLOCK=\"$HOME/_installed/sherlock\"" >> -a .bashrc 
 echo "export PYENV_ROOT=\"$HOME/.pyenv\"" >> -a .bashrc 
 echo "export PATH=\"$PYENV_ROOT/bin:$PATH\"" >> -a .bashrc 
 echo "if command -v pyenv 1>/dev/null 2>&1; then" >> -a .bashrc 
